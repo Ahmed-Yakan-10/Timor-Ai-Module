1d024b991918fea4d43c6d5af43e45e889dd1fec
Ahmed-Yakan-10 <salemdeff@gmail.com>
2024-08-20 21:14:33 +0300

    create custom text field

--- lib/views/chat_bot_view.dart
+++ lib/views/chat_bot_view.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:timor_ai/widgets/custom_text_field.dart';
 
 class ChatBotView extends StatelessWidget {
   const ChatBotView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Expanded(child: ListView()),
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              )),
+          height: 70,
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_photo_alternate_outlined,
+                    color: Colors.white,
+                  )),
+              SizedBox(
+                width: 220,
+                child: CustomTextField(),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.mic_none_outlined,
+                    color: Colors.white,
+                  )),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: Colors.black,
+      style: TextStyle(color: Colors.black),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.all(12),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+}
